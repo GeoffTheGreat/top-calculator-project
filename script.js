4af5a538a8b1e785feab625208d2c0d622e2f1e6
Timothy <reedtimothy63@gmail.com>
2022-07-17 15:02:08 +0200

    add all the logic of treating the operators add all the evenhandlers for the buttons

--- script.js
+++ script.js
@@ -1,1 +1,139 @@
-const screen = document.getElementById("screen");
+let displayOperand = "";
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
+let calculated = 0;
+const calScreen = document.getElementById("screen");
+calScreen.textContent = "0";
+calScreen.style.justifyContent = "flex-end";
+const buttons = Array.from(document.querySelectorAll("button"));
+
+buttons.forEach((button) => {
+  button.addEventListener("click", identify);
+});
+function operLogic() {
+  if (firstOperand === "") {
+    firstOperand = displayOperand;
+  } else {
+    secondOperand = displayOperand;
+  }
+  displayOperand = "";
+  document.getElementById("period").removeAttribute("disabled");
+}
+function hasOperator(receivedOperator) {
+  if (operator === "") {
+    operator = receivedOperator;
+  } else {
+    if (firstOperand === "" || secondOperand === "") {
+      operator = receivedOperator;
+    } else {
+      doCalculation();
+      operator = receivedOperator;
+    }
+  }
+}
+
+function doCalculation() {
+  if (firstOperand !== "" && secondOperand !== "" && operator !== "") {
+    switch (operator) {
+      case "divide":
+        calculated =
+          Math.round(
+            (parseFloat(firstOperand) / parseFloat(secondOperand)) * 100
+          ) / 100;
+        break;
+      case "multiply":
+        calculated =
+          Math.round(
+            parseFloat(firstOperand) * parseFloat(secondOperand) * 100
+          ) / 100;
+        break;
+      case "plus":
+        calculated =
+          Math.round(
+            (parseFloat(firstOperand) + parseFloat(secondOperand)) * 100
+          ) / 100;
+        break;
+      case "minus":
+        calculated =
+          Math.round(
+            parseFloat(firstOperand) * parseFloat(secondOperand) * 100
+          ) / 100;
+        break;
+    }
+    calScreen.textContent = calculated.toString();
+    displayOperand = "";
+    firstOperand = calculated.toString();
+    secondOperand = "";
+    document.getElementById("period").removeAttribute("disabled");
+  }
+}
+function plusMin() {
+  displayOperand = Math.round(parseFloat(displayOperand) * -100) / 100;
+  calScreen.textContent = displayOperand.toString();
+}
+function toPercent() {
+  displayOperand = Math.round(parseFloat(displayOperand) * 100) / 10000;
+  calScreen.textContent = displayOperand.toString();
+}
+
+function identify(e) {
+  let btnId = e.target.id;
+  switch (btnId) {
+    case "nr0":
+    case "nr1":
+    case "nr2":
+    case "nr3":
+    case "nr4":
+    case "nr5":
+    case "nr6":
+    case "nr7":
+    case "nr8":
+    case "nr9":
+      displayOperand += btnId[2];
+      calScreen.textContent = displayOperand;
+      console.log(e);
+      break;
+    case "period":
+      displayOperand = calScreen.textContent;
+      if (displayOperand.indexOf(".") < 0) {
+        displayOperand += ".";
+        e.target.disabled = true;
+      }
+      break;
+    case "divide":
+      operLogic();
+      hasOperator("divide");
+      break;
+    case "multiply":
+      operLogic();
+      hasOperator("multiply");
+      break;
+    case "minus":
+      operLogic();
+      hasOperator("minus");
+      break;
+    case "plus":
+      operLogic();
+      hasOperator("plus");
+      break;
+    case "equals":
+      secondOperand = displayOperand;
+      doCalculation();
+      break;
+    case "clear":
+      firstOperand = "";
+      secondOperand = "";
+      displayOperand = "";
+      operator = "";
+      calculated = 0;
+      calScreen.textContent = "0";
+      break;
+    case "plus-minus":
+      plusMin();
+      break;
+    case "percent":
+      toPercent();
+      break;
+  }
+}
